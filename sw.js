14543ca7229f28bdc7b8540b5e4d72980db9dfa2
ninaplan <womanistkim@gmail.com>
2026-04-05 08:39:47 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'econ-v1';
+const FILES = ['./index.html', './data.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
